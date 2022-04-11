c6814737cbe392566243a0933eee7811ee63f123
Zheng Florent <florentzheng01@gmail.com>
2022-04-11 14:16:40 +0200

    Initial version of myFirstJhipster generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/famoco/myfirstjhipster/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.famoco.myfirstjhipster.config;
---
+++ src/main/java/com/famoco/myfirstjhipster/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.famoco.myfirstjhipster.service.dto;
